1fcaff4ebf25b98a5a02a649986290b4ab032ade
Jose Guzman <jg175415@gmail.com>
2022-05-22 16:21:08 -0500

    feat: adding booyerMoore algorithm

--- src/io/github/joseguzmann/dsaimplementation/Main.java
+++ src/io/github/joseguzmann/dsaimplementation/Main.java
@@ -7,22 +7,38 @@ package io.github.joseguzmann.dsaimplementation;
 
 public class Main {
     public static void main(String[] args) {
-        char[] string1 = new char[7];
-        string1[0] = 'j';
-        string1[1] = 'o';
-        string1[2] = 's';
-        string1[3] = 'e';
-        string1[4] = 'd';
-        string1[5] = 'f';
-        string1[6] = 'g';
+        char[] string1 = new char[20];
+        string1[0] = 'a';
+        string1[1] = 'b';
+        string1[2] = 'a';
+        string1[3] = 'c';
+        string1[4] = 'a';
+        string1[5] = 'a';
+        string1[6] = 'b';
+        string1[7] = 'a';
+        string1[8] = 'd';
+        string1[9] = 'c';
+        string1[10] = 'a';
+        string1[11] = 'b';
+        string1[12] = 'a';
+        string1[13] = 'c';
+        string1[14] = 'a';
+        string1[15] = 'b';
+        string1[16] = 'a';
+        string1[17] = 'a';
+        string1[18] = 'b';
+        string1[19] = 'b';
 
 
-        char[] string2 = new char[3];
-        string2[0] = 'd';
-        string2[1] = 'f';
-        string2[2] = 'g';
+        char[] string2 = new char[6];
+        string2[0] = 'a';
+        string2[1] = 'b';
+        string2[2] = 'a';
+        string2[3] = 'c';
+        string2[4] = 'a';
+        string2[5] = 'b';
 
-        System.out.println(PatternMatching.findBrute(string1, string2));
+        System.out.println(PatternMatching.booyerMoore(string1, string2));
 
     }
 }
--- src/io/github/joseguzmann/dsaimplementation/PatternMatching.java
+++ src/io/github/joseguzmann/dsaimplementation/PatternMatching.java
@@ -1,4 +1,5 @@
 package io.github.joseguzmann.dsaimplementation;
+import java.util.HashMap;
 
 /**
  * @author Jose Guzman
@@ -6,19 +7,63 @@ package io.github.joseguzmann.dsaimplementation;
 
 public class PatternMatching {
     public static int findBrute(char[] text, char[] pattern) {
-        System.out.println("entrar");
         int m = text.length;
         int n = pattern.length;
         for(int i = 0; i <= m - n; i++) {
-            System.out.println("Entrar al for");
             int j = 0;
             while(j < n && pattern[j] == text[i + j]) {
                 j++;
             }
-            if(j == m) {
+            if(j == n) {
                 return i;
             }
         }
         return -1;
+    }
+
+    public static int booyerMoore(char[] texto, char[] patron) {
+        // Definición de longitudes
+        int n = texto.length;
+        int m = patron.length;
+
+        if(m == 0) return 0; // Si el patrón se encuentra vacio
+
+        // Definicion de la tabla de posiciones
+        HashMap<Character, Integer> tabla = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            // Rellenar con todos los carácteres del texto en -1
+            tabla.put(texto[i], -1);
+        }
+        for(int k = 0; k < m; k++) {
+            // Rellenar con todos los caracteres del patrón
+            tabla.put(patron[k], k);
+        }
+
+        // Se empieza a comparar de derecha a izquierda en el patrón
+        int i = m - 1; // Indice en el texto
+        int k = m - 1; // Indice en el patrón
+        int j; // Indice de la ultima ocurrencia del caracter de fallo en el patrón
+
+        while(i < n) {
+            if(texto[i] == patron[k]) {
+                if(k == 0) {
+                    // Si k ya llegó a 0 quiere decir que todos los términos coincidieron
+                    return i;
+                }
+                i--;
+                k--;
+            } else {
+                j = tabla.get(texto[i]); // Evaluación de los dos casos
+                if(j < k) {
+                    i += m - (j + 1); // Si la última ocurrencia esta a la derecha
+                } else if(j > k) {
+                    i += m - k; // Si la última ocurrencia está a la izquierda
+                }
+
+                k = m - 1; // k se reinicia
+            }
+        }
+
+        return -1; // No se encontro el patron
     }
 }
