5de4c5034eb1ebba388d8caf36afca3a9fe455c3
Jose Guzman <jg175415@gmail.com>
2022-05-23 12:04:10 -0500

    feat: adding KMP

--- src/io/github/joseguzmann/dsaimplementation/PatternMatching.java
+++ src/io/github/joseguzmann/dsaimplementation/PatternMatching.java
@@ -21,6 +21,13 @@ public class PatternMatching {
         return -1;
     }
 
+
+    /**
+     *
+     * @param texto
+     * @param patron
+     * @return
+     */
     public static int booyerMoore(char[] texto, char[] patron) {
         // Definición de longitudes
         int n = texto.length;
@@ -55,7 +62,7 @@ public class PatternMatching {
             } else {
                 j = tabla.get(texto[i]); // Evaluación de los dos casos
                 if(j < k) {
-                    i += m - (j + 1); // Si la última ocurrencia esta a la derecha
+                    i += m - (j + 1); // Si la última ocurrencia está a la derecha
                 } else if(j > k) {
                     i += m - k; // Si la última ocurrencia está a la izquierda
                 }
@@ -64,6 +71,66 @@ public class PatternMatching {
             }
         }
 
-        return -1; // No se encontro el patron
+        return -1; // No se encontró el patron
+    }
+
+    /**
+     *
+     * @param texto
+     * @param patron
+     * @return
+     */
+    public static int KMP(char[] texto, char[] patron) {
+        // Definición de las longitudes
+        int n = texto.length;
+        int m = patron.length;
+
+        if(m == 0) return 0; // Si el patrón a buscar está vacío
+
+        // Definimos el array que contiene la función de fallo
+        int[] fallo = funcionFallo(patron);
+
+        // Empezamos por la izquierda
+        int j = 0; // Indice en el texto
+        int k = 0; // Indice en el patron
+
+        while(j < n) {
+            if(texto[j] == patron[k]) {
+                if(k == m - 1) { // Si el patrón ya está completo
+                    return j - m + 1; // Retorna el índice donde empieza el patrón en el texto
+                }
+                j++;
+                k++;
+            } else if(k > 0) {
+                k = fallo[k-1]; // Se reutiliza el sufijo
+            } else {
+                j++; // Se aumenta únicamente el índice en el texto
+            }
+        }
+
+        return -1; // No hay coincidencia
+    }
+
+    public static int[] funcionFallo(char[] patron) {
+        int m = patron.length;
+        int[] fallo = new int[m]; // Definición de la longitud de la funcion de fallo
+
+        // Definición de lo índices
+        int j = 1; // Recorre los sufijos
+        int k = 0; // Recorre los prefijos
+
+        while(j < m) {
+            if(patron[j] == patron[k]) {
+                fallo[j] = k + 1; // Se aumenta el valor de la posicion j en la funcion de fallo
+                j++;
+                k++;
+            } else if(k > 0) {
+                // la posición de j se mantiene y k retrocede para seguir construyendo un patron
+                k = fallo[k-1];
+            } else {
+                j++; // la posición de j aumenta
+            }
+        }
+        return fallo;
     }
 }
